5672f6defa25077b7803490afd3bb217cacedfbb
bharan <bharansooraj@gmail.com>
2025-12-02 23:09:20 +0530

    New page forked from scholarsite

--- script.js
+++
@@ -1,9 +0,0 @@
-function openSideBar() {
-  document.getElementById("sideBar").classList.remove("collapsed");
-  document.getElementById("overlay").classList.remove("hidden");
-}
-
-function closeSideBar() {
-  document.getElementById("sideBar").classList.add("collapsed");
-  document.getElementById("overlay").classList.add("hidden");
-}
